a141b32f54a49b8611cff9ad921aeb4ad133e6ad
Michael Bestas <mkbestas@lineageos.org>
2024-12-05 03:51:50 +0200

    Add oem-iptables-init.sh for Wi-Fi calling in airplane mode
    Change-Id: Ice696ee2efaf535dc80cdeb54c7d190c6dbaca62

--- shiba-vendor.mk
+++ shiba-vendor.mk
@@ -490,6 +490,7 @@ PRODUCT_PACKAGES += \
     google-ril \
     oemrilhook \
     vendor.google.edgetpu_app_service@1.0-service.xml \
+    oem-iptables-init \
     vendor.google.edgetpu_app_service@1.0-service
 
 PRODUCT_PACKAGES += \
